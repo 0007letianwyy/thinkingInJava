ef558a1adf1b77c7ac9da1719ba8aa2958ac38b5
0007letianwyy <d007letian@163.com>
2018-10-22 18:38:34 +0800

    test

--- src/main/java/com/knight/study_project/StudyProjectApplication.java
+++ src/main/java/com/knight/study_project/StudyProjectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudyProjectApplication {
-    //test
 
     public static void main(String[] args) {
         SpringApplication.run(StudyProjectApplication.class, args);
