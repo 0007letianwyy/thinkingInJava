05b38c5f2a76478212d1fad14e6bf7a5be6c74d1
0007letianwyy <d007letian@163.com>
2018-11-02 18:56:43 +0800

    内部类的继承

---
+++ src/main/java/com/knight/study_project/inner/BigEgg2.java
@@ -0,0 +1,16 @@
+package com.knight.study_project.inner;
+
+/**
+ * 明确地继承内部类
+ *
+ * @Author: knight
+ * @Date: 2018/11/2 下午5:43
+ */
+class Egg2 {
+    private class Yolk{
+
+    }
+}
+
+public class BigEgg2 {
+}
