da3a0aa5e402d6c26c060afc5fbe6e26d155fea9
0007letianwyy <d007letian@163.com>
2018-11-05 17:43:58 +0800

    内部类

--- src/main/java/com/knight/study_project/inner/BigEgg2.java
+++ src/main/java/com/knight/study_project/inner/BigEgg2.java
@@ -7,10 +7,46 @@ package com.knight.study_project.inner;
  * @Date: 2018/11/2 下午5:43
  */
 class Egg2 {
-    private class Yolk{
+    protected class Yolk {
+        public Yolk() {
+            System.out.println("Egg2.Yolk()");
+        }
 
+        public void f() {
+            System.out.println("Egg2.Yolk.f()");
+        }
+    }
+
+    private Yolk y = new Yolk();
+
+    public Egg2() {
+        System.out.println("new Egg2()");
+    }
+
+    public void insertYolk(Yolk yolk) {
+        y = yolk;
+    }
+
+    public void g() {
+        y.f();
     }
 }
 
-public class BigEgg2 {
+public class BigEgg2 extends Egg2{
+    public class Yolk extends Egg2.Yolk{
+        public Yolk(){
+            System.out.println("BigEgg2.Yolk()");
+        }
+        public void f(){
+            System.out.println("BigEgg2.Yolk.f()");
+        }
+    }
+    public BigEgg2(){
+        insertYolk(new Yolk());
+    }
+
+    public static void main(String[] args) {
+        Egg2 egg2 = new BigEgg2();
+        egg2.g();
+    }
 }
